347699cd181b0997738cd299f3d384efdd13d607
akira noguchi <a_noguchi@libertyfish.co.jp>
2017-09-24 18:37:24 +0900

    add some config for config/deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,36 +1,55 @@
 # config valid only for current version of Capistrano
 lock "3.9.1"
 
-set :application, "my_app_name"
-set :repo_url, "git@example.com:me/my_repo.git"
+server ENV['AWESOME_IP_ADDR'], port: 2123, roles: [:app, :web, :db], primary: true
 
-# Default branch is :master
-# ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
+set :application, "cap-rails"
+set :repo_url, "https://github.com/guppy0356/cap-rails.git"
 
-# Default deploy_to directory is /var/www/my_app_name
-# set :deploy_to, "/var/www/my_app_name"
+# user
+set :user,            'deploy'
+set :use_sudo,        false
 
-# Default value for :format is :airbrussh.
-# set :format, :airbrussh
+# puma
+set :puma_threads,    [4, 16]
+set :puma_workers,    0
+set :puma_bind,       "unix://#{shared_path}/tmp/sockets/#{fetch(:application)}-puma.sock"
+set :puma_state,      "#{shared_path}/tmp/pids/puma.state"
+set :puma_pid,        "#{shared_path}/tmp/pids/puma.pid"
+set :puma_access_log, "#{release_path}/log/puma.access.log"
+set :puma_error_log,  "#{release_path}/log/puma.error.log"
+set :puma_preload_app, true
+set :puma_worker_timeout, nil
+set :puma_init_active_record, true
 
-# You can configure the Airbrussh format using :format_options.
-# These are the defaults.
-# set :format_options, command_output: true, log_file: "log/capistrano.log", color: :auto, truncate: :auto
+# terminal
+set :pty,             true
 
-# Default value for :pty is false
-# set :pty, true
+# server
+set :stage,           :production
+set :deploy_via,      :remote_cache
+set :deploy_to,       "/var/www/rails/#{fetch(:application)}"
 
-# Default value for :linked_files is []
-# append :linked_files, "config/database.yml", "config/secrets.yml"
+# ssh
+set :ssh_options,     {
+  user: 'deploy'
+}
 
-# Default value for linked_dirs is []
-# append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "public/system"
+# rvm
+set :rvm_ruby_version, 'ruby-2.4.1@cap-rails'
 
-# Default value for default_env is {}
-# set :default_env, { path: "/opt/ruby/bin:$PATH" }
-
-# Default value for local_user is ENV['USER']
-# set :local_user, -> { `git config user.name`.chomp }
-
-# Default value for keep_releases is 5
-# set :keep_releases, 5
+# environment
+# ref https://github.com/capistrano/rails#symlinks
+set :linked_dirs, fetch(:linked_dirs, []).push(
+  'log',
+  'tmp/pids',
+  'tmp/cache',
+  'tmp/sockets',
+  'vendor/bundle',
+  'public/system',
+  'public/uploads'
+)
+set :linked_files, fetch(:linked_files, []).push(
+  'config/database.yml',
+  'config/secrets.yml'
+)
